625a050293b76a73bea6737319f9235af90bdd92
petestapp <63821853+petestapp@users.noreply.github.com>
2021-12-31 19:29:42 -0600

    started on show location details modal

--- src/components/ListDetails/ListDetails.jsx
+++ src/components/ListDetails/ListDetails.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import listdetails from '../../redux/reducers/listdetails.reducer';
 
 function ListDetails(props) {
   const dispatch = useDispatch();
@@ -30,7 +31,7 @@ function ListDetails(props) {
     listID: list.id
   })
 
-  const handleChange = (event) => {
+  const handleChangeNewLocation = (event) => {
     setNewLocation({ ...newLocation, [event.target.name]: event.target.value });
   }
 
@@ -40,10 +41,23 @@ function ListDetails(props) {
       type: 'ADD_NEW_LOCATION',
       payload: newLocation
     })
+    setNewLocation({
+      name: '',
+      city: '',
+      state: '',
+      zip: 0,
+      latitude: 0,
+      longitude: 0,
+      rating: '',
+      comments: '',
+    })
   }
 
-  const consoleLogNewLocation = () => {
-    console.log('newLocation:', newLocation);
+  const [clickedLocationIndex, setClickedLocationIndex] = useState(0);
+
+  const setClickedLocation = (index) => {
+    setClickedLocationIndex(index);
+    console.log('clickedLocationIndex:', clickedLocationIndex)
   }
 
   return (
@@ -62,16 +76,28 @@ function ListDetails(props) {
         <tbody>
           {listDetails.map((place, index) => (
             <tr key={index}>
-              <td>{place.name}</td>
+              <td>{index}, {place.name}</td>
               <td>{place.city}, {place.state}</td>
               <td>{place.rating}</td>
               <td>
-                <button class="btn btn-outline-primary">More Info</button>
+                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#showLocationDetails" onClick={() => setClickedLocation(index)}>
+                  More Info
+                </button>
               </td>
             </tr>
           ))}
         </tbody>
       </table>
+
+      <div class="modal fade" id="showLocationDetails" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              {listDetails[clickedLocationIndex].name}
+            </div>
+          </div>
+        </div>
+      </div>
 
       <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addNewLocation">
         Add New Location
@@ -89,17 +115,17 @@ function ListDetails(props) {
                 <label>
                   <div class="mb-3">
                     <label class="form-label">Name</label>
-                    <input class="form-control" type="text" name="name" placeholder="name" onChange={(event) => handleChange(event)} />
+                    <input class="form-control" type="text" name="name" placeholder="name" onChange={(event) => handleChangeNewLocation(event)} />
                   </div>
 
                   <div class="row">
                     <div class="col-md-5 mb-3">
                       <label class="form-label">City</label>
-                      <input class="form-control" type="text" name="city" placeholder="city" onChange={(event) => handleChange(event)} />
+                      <input class="form-control" type="text" name="city" placeholder="city" onChange={(event) => handleChangeNewLocation(event)} />
                     </div>
                     <div class="col-md-4 mb-3">
                       <label class="form-label">State</label>
-                      <select class="form-select" name="state" placeholder="State" onChange={(event) => handleChange(event)}>
+                      <select class="form-select" name="state" placeholder="State" onChange={(event) => handleChangeNewLocation(event)}>
                         <option selected>State</option>
                         <option value="AL">Alabama</option>
                         <option value="AK">Alaska</option>
@@ -153,26 +179,26 @@ function ListDetails(props) {
                     </div>
                     <div class="col-md-3 mb-3">
                       <label class="form-label">ZIP Code</label>
-                      <input class="form-control" type="text" name="zip" placeholder="ZIP code" onChange={(event) => handleChange(event)} />
+                      <input class="form-control" type="text" name="zip" placeholder="ZIP code" onChange={(event) => handleChangeNewLocation(event)} />
                     </div>
                   </div>
                   <div class="row">
                     <div class="col-md-4 mg-3">
                       <label class="form-label">Latitude</label>
-                      <input class="form-control" type="text" name="latitude" placeholder="latitude" onChange={(event) => handleChange(event)} />
+                      <input class="form-control" type="text" name="latitude" placeholder="latitude" onChange={(event) => handleChangeNewLocation(event)} />
                     </div>
                     <div class="col-md-4 mb-3">
                       <label class="form-label">Longitude</label>
-                      <input class="form-control" type="text" name="longitude" placeholder="longitude" onChange={(event) => handleChange(event)} />
+                      <input class="form-control" type="text" name="longitude" placeholder="longitude" onChange={(event) => handleChangeNewLocation(event)} />
                     </div>
                     <div class="col-md-4 mb-3">
                       <label class="form-label">Rating</label>
-                      <input class="form-control" type="text" name="rating" placeholder="rating" onChange={(event) => handleChange(event)} />
+                      <input class="form-control" type="text" name="rating" placeholder="rating" onChange={(event) => handleChangeNewLocation(event)} />
                     </div>
                   </div>
                   <div class="mb-3">
                     <label class="form-label">Comments</label>
-                    <textarea name="comments" placeholder="comments" onChange={(event) => handleChange(event)} class="form-control" rows="3"></textarea>
+                    <textarea name="comments" placeholder="comments" onChange={(event) => handleChangeNewLocation(event)} class="form-control" rows="3"></textarea>
                   </div>
                 </label>
                 <div class="modal-footer">
@@ -189,69 +215,3 @@ function ListDetails(props) {
 }
 
 export default ListDetails;
-
-// import React, { useState } from 'react';
-// import { useDispatch, useSelector } from 'react-redux';
-
-// function AddNewLocation(props) {
-//   const dispatch = useDispatch();
-//   const store = useSelector((store) => store);
-//   const [heading, setHeading] = useState('Add New Location');
-//   const list = store.list;
-//   const user = store.user;
-
-//   const [newLocation, setNewLocation] = useState({
-//     name: '',
-//     city: '',
-//     state: '',
-//     zip: 0,
-//     latitude: 0,
-//     longitude: 0,
-//     rating: '',
-//     comments: '',
-//     userID: user.id,
-//     listID: list.id
-//   })
-
-//   const handleChange = (event) => {
-//     setNewLocation({...newLocation, [event.target.name]: event.target.value});
-//   }
-
-//   const submitNewLocation = () => {
-//     event.preventDefault();
-//     dispatch({
-//       type: 'ADD_NEW_LOCATION',
-//       payload: newLocation
-//     })
-//   }
-
-//   const consoleLogNewLocation = () => {
-//     console.log('newLocation:', newLocation);
-//   }
-
-//   return (
-//     <div>
-//       <h2>{heading}</h2>
-//       <h3>User ID: {user.id}</h3>
-//       <h4>{list.name}</h4>
-//       <button onClick={consoleLogNewLocation}>console.log newLocation</button>
-      // <form onSubmit={submitNewLocation}>
-      //   <label>
-      //     New Location:
-      //     <input type="text" name="name" placeholder="Location Name" onChange={(event)=>handleChange(event)}/>
-      //     <input type="text" name="city" placeholder="City" onChange={(event)=>handleChange(event)}/>
-      //     <input type="text" name="state" placeholder="State" onChange={(event)=>handleChange(event)}/>
-      //     <input type="number" name="zip" placeholder="ZIP Code" onChange={(event)=>handleChange(event)}/>
-      //     <input type="number" name="latitude" placeholder="Latitude" onChange={(event)=>handleChange(event)}/>
-      //     <input type="number" name="longitude" placeholder="Longitude" onChange={(event)=>handleChange(event)}/>
-      //     <br/>
-      //     <input type="text" name="rating" placeholder="rating" onChange={(event)=>handleChange(event)}/>
-      //     <input type="text" name="comments" placeholder="comments" onChange={(event)=>handleChange(event)}/>
-      //   </label>
-      //   <input type="submit"/>
-      // </form>
-//     </div>
-//   );
-// }
-
-// export default AddNewLocation;
